eb712bbb572c7265289acaba57eaef4da06ac577
ukf-milengyel <114596047+ukf-milengyel@users.noreply.github.com>
2023-12-02 17:29:47 +0100

    something resembling a game

---
+++ Fruit.js
@@ -0,0 +1,28 @@
+class GameObject {
+    update() {}
+}
+
+class PhysicsObject extends GameObject {
+    constructor(x, y, xSpeed, ySpeed, gravity, image) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.gravity = gravity;
+        this.image = image;     // redundant?
+    }
+
+    update(delta) {
+        this.x += this.xSpeed * delta;
+        this.y += this.ySpeed * delta;
+        this.ySpeed += this.gravity * delta;
+    }
+}
+
+class Fruit extends PhysicsObject {
+    constructor(x, y, xSpeed, ySpeed, gravity, image, score) {
+        super(x, y, xSpeed, ySpeed, gravity, image);
+        this.score = score;
+    }
+}
--- game.js
+++ game.js
@@ -1,6 +1,9 @@
-var gridCanvas = document.querySelector('#game-container');
-
+var gridCanvas = document.querySelector('#webcam-trail');
 var gridCtx = gridCanvas.getContext('2d');
+
+var gameCanvas = document.querySelector('#game-container');
+var ctx = gameCanvas.getContext('2d');
+
 var gridWidth = 1040;
 var gridHeight = 800;
 var resolutionX = 15;
@@ -9,7 +12,16 @@ var cellWidth = gridWidth / resolutionX;
 var cellHeight = gridHeight / resolutionY;
 var PI = Math.PI;
 
+const TIMESCALE = 1.0;
+const GRAVITY = 0.0025;
+const mX = 1;
+const mXD = mX/2;
+const mY = 1;
+const mYD = mY * 0.75;   
+
 var radius = 0;
+
+let previousTimestamp;        // delta calculation
 
 function drawGrid(matrix) {
   gridCtx.clearRect(0,0,gridWidth, gridHeight);
@@ -45,11 +57,79 @@ mediaDevices
         audio: true,
     })
     .then((stream) => {
-    
-        // Changing the source of video to current stream.
         video.srcObject = stream;
         video.addEventListener("loadedmetadata", () => {
             video.play();
         });
     })
     .catch(alert);
+
+
+let fruits;
+let slices;
+let timeLeft;
+
+function isOutOfBounds(object) {
+    return object.y <= gridHeight;
+}
+
+function init() {
+    fruits = [];
+    slices = [];
+    timeLeft = 120;
+    ctx.fillStyle = "red";
+}
+
+function update(timeStamp) {
+    // ---- delta calculation
+    if (previousTimestamp === undefined) {
+        previousTimestamp = timeStamp;
+    }
+    const delta = (timeStamp - previousTimestamp) * TIMESCALE;
+    previousTimestamp = timeStamp;
+    // ----
+
+    fruits = fruits.filter(function (fruit) {
+        return isOutOfBounds(fruit);
+    });
+
+    ctx.clearRect(0,0,gridWidth,gridHeight);
+    for (fruit of fruits) {
+        fruit.update(delta);
+        ctx.beginPath();
+        ctx.rect(fruit.x, fruit.y, 50, 50);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    requestAnimationFrame(update);
+}
+
+function timerTick() {
+    timeLeft -= 1;
+    if(timeLeft <= 0) {
+        // Game over
+    }
+}
+
+function gameOver() {
+
+}
+
+function spawnFruit() {
+    const amount = Math.ceil(Math.random() * 3);
+    for (let i = 0; i < amount; i++) {
+        fruits.unshift(new Fruit(
+            Math.random() * gridWidth, gridHeight - 1, 
+            Math.random() * mX - mXD , -(mYD + Math.random() * mY), GRAVITY, "", 10
+        ));
+    }
+}
+
+
+
+init();
+
+setInterval(timerTick, 1000);
+setInterval(spawnFruit, 1250);
+requestAnimationFrame(update);

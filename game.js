ea14fc9dabe76add0560a6e303d43bb5f7a53827
miro <miroslav.lengyel@student.ukf.sk>
2023-12-02 18:24:33 +0100

    add fruit images, timer and score display

--- game.js
+++ game.js
@@ -1,23 +1,28 @@
-var gridCanvas = document.querySelector('#webcam-trail');
-var gridCtx = gridCanvas.getContext('2d');
+let gridCanvas = document.querySelector('#webcam-trail');
+let gridCtx = gridCanvas.getContext('2d');
 
-var gameCanvas = document.querySelector('#game-container');
-var ctx = gameCanvas.getContext('2d');
+let gameCanvas = document.querySelector('#game-container');
+let ctx = gameCanvas.getContext('2d');
 
-var gridWidth = 1040;
-var gridHeight = 800;
-var resolutionX = 15;
-var resolutionY = 15;
-var cellWidth = gridWidth / resolutionX;
-var cellHeight = gridHeight / resolutionY;
-var PI = Math.PI;
+let gridWidth = 800;
+let gridHeight = 600;
+let resolutionX = 15;
+let resolutionY = 15;
+let cellWidth = gridWidth / resolutionX;
+let cellHeight = gridHeight / resolutionY;
+let PI = Math.PI;
 
 const TIMESCALE = 1.0;
 const GRAVITY = 0.0025;
 const mX = 1;
 const mXD = mX/2;
 const mY = 1;
-const mYD = mY * 0.75;   
+const mYD = mY * 0.75;  
+
+const fruitSize = 60;
+
+const imageTypes = ["baklazan", "mrkva", "paradajka", "tekvica", "uhorka"];
+const images = {};
 
 var radius = 0;
 
@@ -43,8 +48,8 @@ const diffy = Diffy.create({
     resolution: { x: resolutionX, y: resolutionY },
     sensitivity: 0.05,
     threshold: 90,
-    debug: true,
-    containerClassName: 'diffy-demo',
+    //debug: true,
+    //containerClassName: 'diffy-demo',
     sourceDimensions: { w: 130, h: 100 },
     onFrame: /*function (matrix) {}*/ drawGrid
   });
@@ -78,6 +83,10 @@ function init() {
     slices = [];
     timeLeft = 120;
     ctx.fillStyle = "red";
+
+    setInterval(timerTick, 1000);
+    setInterval(spawnFruit, 1250);
+    requestAnimationFrame(update);
 }
 
 function update(timeStamp) {
@@ -96,10 +105,8 @@ function update(timeStamp) {
     ctx.clearRect(0,0,gridWidth,gridHeight);
     for (fruit of fruits) {
         fruit.update(delta);
-        ctx.beginPath();
-        ctx.rect(fruit.x, fruit.y, 50, 50);
-        ctx.fill();
-        ctx.stroke();
+        const img = fruit.image
+        ctx.drawImage(images[fruit.image][0], fruit.x, fruit.y, fruitSize, fruitSize);
     }
 
     requestAnimationFrame(update);
@@ -107,6 +114,7 @@ function update(timeStamp) {
 
 function timerTick() {
     timeLeft -= 1;
+    document.getElementById("time-display").innerText = timeLeft;
     if(timeLeft <= 0) {
         // Game over
     }
@@ -120,16 +128,22 @@ function spawnFruit() {
     const amount = Math.ceil(Math.random() * 3);
     for (let i = 0; i < amount; i++) {
         fruits.unshift(new Fruit(
-            Math.random() * gridWidth, gridHeight - 1, 
-            Math.random() * mX - mXD , -(mYD + Math.random() * mY), GRAVITY, "", 10
+            gridWidth /4 + (Math.random() * gridWidth /2), gridHeight - 1, 
+            Math.random() * mX - mXD , -(mYD + Math.random() * mY), GRAVITY, Math.floor(Math.random()*imageTypes.length), 10
         ));
     }
 }
 
+function loadImages() {
+    for (imageType in imageTypes) {
+        images[imageType] = [];
+        for (let i = 0; i < 3; i++) {
+            const img = document.createElement("img");
+            img.src = "./images/" + imageTypes[imageType] + i + ".png";
+            images[imageType][i] = img;
+        }
+    }
+}
 
-
-init();
-
-setInterval(timerTick, 1000);
-setInterval(spawnFruit, 1250);
-requestAnimationFrame(update);
+loadImages();
+setTimeout(init, 1000); // call to start the game
